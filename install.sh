20d26b28022a9ed655f020ee11f8a171853ce19d
cheryamme <cheryamme@163.com>
2022-08-03 11:36:13 +0800

    😁add .p10k.zsh

--- install.sh
+++ install.sh
@@ -1,3 +1,2 @@
 
-cp .key-bindings.zsh .zshrc .bashrc .fzf.bash .fzf.zsh .radian_profile .Rprofile .tmux.conf .tmux.conf.local ~
-
+ln -s .p10k.zsh .key-bindings.zsh .zshrc .bashrc .fzf.bash .fzf.zsh .radian_profile .Rprofile .tmux.conf .tmux.conf.local ~
